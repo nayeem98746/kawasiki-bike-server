cae1f2d8d60fe122704a757a95a8630c1b111dd6
nayeem407 <sabbirahmednayeem941@gmail.com>
2022-05-07 23:17:09 +0600

    payment system

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
+const SSLCommerzPayment = require('sslcommerz')
+const ObjectId = require('mongodb').ObjectId;
+const { v4: uuidv4 } = require('uuid');
+
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -60,6 +64,13 @@ async function run(){
           console.log(result)
           res.json(result)
         })
+        app.get('/explore2/:id', async (req, res)=> {
+          const id = req.params.id
+          console.log('getting specific service' , id)
+          const query ={_id: ObjectId(id)};
+          const service = await explore2Collection.findOne(query);
+          res.json(service)
+       })
 
         // review post
         app.post('/addReview', async (req,res)=> {
@@ -90,7 +101,7 @@ async function run(){
           const result = await usersCollection.updateOne(filter,updateDoc)
           res.json(result)
         })
-        app.get('/users/:email', async(req, res)=> {
+         app.get('/users/:email', async(req, res)=> {
           const email= req.params.email
           const query = {email: email}
           const user = await usersCollection.findOne(query)
@@ -132,8 +143,81 @@ async function run(){
       })
         
 
-
-
+      // payment initialization api
+      app.post('/init',async(req, res) => {
+        console.log(req.body);
+        const data = {
+            total_amount: req.body.total_amount,
+            currency: 'BDT',
+            tran_id: uuidv4(),
+            success_url: 'http://localhost:5000/success',
+            fail_url: 'http://localhost:5000/fail',
+            cancel_url: 'http://localhost:5000/cancel',
+            ipn_url: 'http://localhost:5000/ipn',
+            shipping_method: 'Courier',
+            paymentStatus:'pending',
+            product_name: req.body.product_name,
+            product_category: 'Electronic',
+            product_image: req.body.product_image,
+            product_profile: req.body.product_profile,
+            cus_name: req.body.cus_name,
+            cus_email: req.body.cus_email,
+            cus_add1: 'Dhaka',
+            cus_add2: 'Dhaka',
+            cus_city: 'Dhaka',
+            cus_state: 'Dhaka',
+            cus_postcode: '1000',
+            cus_country: 'Bangladesh',
+            cus_phone: '01711111111',
+            cus_fax: '01711111111',
+            ship_name: 'Customer Name',
+            ship_add1: 'Dhaka',
+            ship_add2: 'Dhaka',
+            ship_city: 'Dhaka',
+            ship_state: 'Dhaka',
+            ship_postcode: 1000,
+            ship_country: 'Bangladesh',
+            multi_card_name: 'mastercard',
+            value_a: 'ref001_A',
+            value_b: 'ref002_B',
+            value_c: 'ref003_C',
+            value_d: 'ref004_D'
+        };
+        console.log(data);
+        // Insert Order data 
+        const order = await orderCollection.insertOne(data)
+        const sslcommer = new SSLCommerzPayment(process.env.Store_ID, process.env.Store_Password,false) //true for live default false for sandbox
+        sslcommer.init(data).then(data => {
+            //process the response that got from sslcommerz 
+            //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+           if(data.GatewayPageURL){
+            res.json(data.GatewayPageURL)
+           }
+           else{
+             return res.status(400)
+             massage:'Payment session faild'
+           }
+        });
+    })
+    app.post('/success' , async(req,res) => {
+        console.log(req.body)
+       const order = await orderCollection.updateOne({tran_id: req.body.tran_id},{
+        $set:{
+          val_id:req.body.val_id
+        }
+       })
+        res.status(200).redirect(`https://niche-webside.web.app/success`)
+    })
+    app.post('/fail' , async(req,res) => {
+        console.log(req.body)
+        const order = await orderCollection.deleteOne({tran_id:req.body.tran_id})
+        res.status(400).redirect(`https://niche-webside.web.app`)
+    })
+    app.post('/cancel' , async(req,res) => {
+        console.log(req.body)
+        res.status(200).redirect(`https://niche-webside.web.app`)
+    })
+    
 
 
     }
